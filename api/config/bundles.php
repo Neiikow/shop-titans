216b3b22a7278e73fdf2067d8164c75fe2ce6158
Neiikow <bergerjordan24@gmail.com>
2020-06-14 16:59:02 +0200

    [API] Install bundle SensioFrameworkExtraBundle. Permet d'utiliser le body converteur de FOSRest

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
