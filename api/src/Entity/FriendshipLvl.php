a313603dbe5f1683a564a6e82ff688f2fd6fde2d
Neiikow <bergerjordan24@gmail.com>
2020-06-14 14:31:13 +0200

    [API] Create FriendshipLvl entity. Répertorie les informations liées aux différents niveaux d'amitiés

---
+++ api/src/Entity/FriendshipLvl.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FriendshipLvlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FriendshipLvlRepository::class)
+ */
+class FriendshipLvl
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $lvl;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $rankName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nrgBonus;
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getLvl(): ?int { return $this->lvl; }
+
+    public function setLvl(int $lvl): self
+    {
+        $this->lvl = $lvl;
+        return $this;
+    }
+
+    public function getRankName(): ?string { return $this->rankName; }
+
+    public function setRankName(string $rankName): self
+    {
+        $this->rankName = $rankName;
+        return $this;
+    }
+
+    public function getNrgBonus(): ?int { return $this->nrgBonus; }
+
+    public function setNrgBonus(int $nrgBonus): self
+    {
+        $this->nrgBonus = $nrgBonus;
+        return $this;
+    }
+}
