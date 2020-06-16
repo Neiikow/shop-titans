63b7a647fe6166d23eb9e5304822f5f92be9ad5c
Neiikow <bergerjordan24@gmail.com>
2020-06-16 13:58:46 +0200

    [API] Create ItemCategory entity. Répertorie les informations liées aux catégories d'objets

---
+++ api/src/Entity/ItemCategory.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemCategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ItemCategoryRepository::class)
+ */
+class ItemCategory
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $img;
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getName(): ?string { return $this->name; }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getImg(): ?string { return $this->img; }
+
+    public function setImg(?string $img): self
+    {
+        $this->img = $img;
+        return $this;
+    }
+}
