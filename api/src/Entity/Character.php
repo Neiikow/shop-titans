a5512c8a0e3fa00ea278176f3a311de9337585ff
Neiikow <bergerjordan24@gmail.com>
2020-06-16 13:42:58 +0200

    [API] Create Character entity. Répertorie les informations liées aux personnages

---
+++ api/src/Entity/Character.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=CharacterRepository::class)
+ * @ORM\Table(name="`character`")
+ */
+class Character
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $type;
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getName(): ?string { return $this->name; }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getImg(): ?string { return $this->img; }
+
+    public function setImg(?string $img): self
+    {
+        $this->img = $img;
+        return $this;
+    }
+
+    public function getType(): ?string { return $this->type; }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+}
