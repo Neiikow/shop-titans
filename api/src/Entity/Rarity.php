7d3880c0ab552e57510d0f62f3c03ac1c5e69c83
Neiikow <bergerjordan24@gmail.com>
2020-06-17 13:47:03 +0200

    [API] Create Rarity entity. Répertorie les informations liées aux raretés

---
+++ api/src/Entity/Rarity.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RarityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=RarityRepository::class)
+ */
+class Rarity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Length(max="255")
+     * @Assert\Type("string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Length(max="255")
+     * @Assert\Type("string")
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Length(max="255")
+     * @Assert\Type("string")
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="integer")
+     * 
+     * @JMS\Type("strict_integer")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Type("integer")
+     */
+    private $tier;
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getName(): ?string { return $this->name; }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getImg(): ?string { return $this->img; }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+        return $this;
+    }
+
+    public function getColor(): ?string { return $this->color; }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function getTier(): ?int { return $this->tier; }
+
+    public function setTier(int $tier): self
+    {
+        $this->tier = $tier;
+        return $this;
+    }
+}
