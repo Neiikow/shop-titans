12586fbf9a2613c9f68e41796592cb45f3dd926a
Neiikow <bergerjordan24@gmail.com>
2020-06-17 13:36:01 +0200

    [API] Create Consumable entity. Répertorie les informations liées aux consommables

---
+++ api/src/Entity/Consumable.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsumableRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ConsumableRepository::class)
+ */
+class Consumable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @JMS\Type("strict_string")
+     * 
+     * @Assert\Type("string")
+     * @Assert\Length(max="255")
+     */
+    private $effect;
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getName(): ?string { return $this->name; }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): ?string { return $this->description; }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getImg(): ?string { return $this->img; }
+
+    public function setImg(?string $img): self
+    {
+        $this->img = $img;
+        return $this;
+    }
+
+    public function getEffect(): ?string { return $this->effect; }
+
+    public function setEffect(?string $effect): self
+    {
+        $this->effect = $effect;
+        return $this;
+    }
+}
