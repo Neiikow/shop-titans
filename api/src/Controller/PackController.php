819c77bb2e3a55224c7a5d9ac0408f2d10a23093
Neiikow <bergerjordan24@gmail.com>
2020-06-17 14:45:31 +0200

    [API] Create Pack controlleur. Répertorie les méthodes de manipulation de l'entité Pack

---
+++ api/src/Controller/PackController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PackController extends AbstractController
+{
+    /**
+     * @Route("/pack", name="pack")
+     */
+    public function index()
+    {
+        return $this->render('pack/index.html.twig', [
+            'controller_name' => 'PackController',
+        ]);
+    }
+}
