3c1c0d73ca6b9ebe5225cdad56e98e3ea1406fea
Neiikow <bergerjordan24@gmail.com>
2020-06-23 14:23:45 +0200

    [API] Update BuildingLvl controller. Ajout du CRUD pour pouvoir manipuler l'entité

--- api/src/Controller/BuildingLvlController.php
+++ api/src/Controller/BuildingLvlController.php
@@ -2,18 +2,148 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\BuildingLvl;
+use App\Method\EntityRelation;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use App\Exception\ResourceValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
-class BuildingLvlController extends AbstractController
+class BuildingLvlController extends FOSRestController
 {
     /**
-     * @Route("/building/lvl", name="building_lvl")
+     * @Rest\Post(
+     *    path = "/api/admin/building/lvl/create",
+     *    name = "buildingLvl_create"
+     * )
+     * @Rest\View(StatusCode = 201, serializerEnableMaxDepthChecks=true)
+     * @ParamConverter("lvl", converter="fos_rest.request_body")
      */
-    public function index()
+    public function create(BuildingLvl $lvl, ConstraintViolationList $violations)
     {
-        return $this->render('building_lvl/index.html.twig', [
-            'controller_name' => 'BuildingLvlController',
-        ]);
+
+        if (count($violations)) {
+            $message = 'Données reçues non valides :';
+            foreach ($violations as $violation) {
+                $message .= sprintf(" %s : %s", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new ResourceValidationException($message);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $er = new EntityRelation;
+
+        $er->createManyToOne($em, $lvl, "building", "Building");
+        
+        $em->persist($lvl);
+        $em->flush();
+
+        return $this->view(
+            $lvl,
+            Response::HTTP_CREATED,
+            ['Location' =>$this->generateUrl(
+                'buildingLvl_id',
+                ['id' => $lvl->getId()])
+            ]
+        );
+    }
+
+    /**
+     * @Rest\Post(
+     *    path = "/api/admin/building/lvl/update/{id}",
+     *    name = "buildingLvl_update",
+     *    requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     * @ParamConverter("lvl", converter="fos_rest.request_body")
+     */
+    public function update($id, BuildingLvl $lvl, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'Données reçues non valides :';
+            foreach ($violations as $violation) {
+                $message .= sprintf(" %s : %s", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new ResourceValidationException($message);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $data = $em->getRepository(BuildingLvl::class)->find($id);        
+
+        if (!$data) {
+            throw $this->createNotFoundException(
+                'Niveau introuvable'
+            );
+        }
+
+        $data->setLvl($lvl->getLvl());
+        $data->setTickNeeded($lvl->getTickNeeded());
+        $data->setEffect($lvl->getEffect());
+        
+        $em->flush();
+
+        return $data;
+    }
+
+    /**
+     * @Rest\Delete(
+     *    path = "/api/admin/building/lvl/delete/{id}",
+     *    name = "buildingLvl_delete",
+     *    requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     */
+    public function delete($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lvl = $em->getRepository(BuildingLvl::class)->find($id);
+
+        if (!$lvl) {
+            throw $this->createNotFoundException(
+                'Niveau introuvable'
+            );
+        }
+
+        $em->remove($lvl);
+        $em->flush();
+
+        return $lvl;
+    }
+    
+    /**
+     * @Rest\Get(
+     *    path = "/api/building/lvl/{id}",
+     *    name = "buildingLvl_id",
+     *    requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     */
+    public function readId($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $lvl = $em->getRepository(BuildingLvl::class)->find($id);
+
+        if (!$lvl) {
+            throw $this->createNotFoundException(
+                'Niveau introuvable'
+            );
+        }
+        return $lvl;
+    }
+
+    /**
+     * @Rest\Get(
+     *    path = "/api/building/lvl",
+     *    name = "buildingLvl_list",
+     * )
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     */
+    public function readAll()
+    {
+        $lvl = $this->getDoctrine()->getRepository('App:BuildingLvl')->findAll();
+
+        return $lvl;
     }
 }
